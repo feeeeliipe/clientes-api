7273c9842fe1dc068dedab865f3c34837b6a75cb
feeeeliipe <felipe.rinaldi.goncalves@gmail.com>
2018-07-20 15:08:00 -0300

    Commit

---
+++ src/main/java/br/com/felipe/clientesapi/model/Tarefa.java
@@ -0,0 +1,47 @@
+package br.com.felipe.clientesapi.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Tarefa {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne
+	private Cliente cliente;
+	
+	private String descricao;
+	private boolean concluida;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Cliente getCliente_id() {
+		return cliente;
+	}
+	public void setCliente_id(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public boolean isConcluida() {
+		return concluida;
+	}
+	public void setConcluida(boolean concluida) {
+		this.concluida = concluida;
+	}
+	
+	
+}
